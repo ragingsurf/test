ad543d114299615dea3c1a0de3cd3ec8fb483c40
ragingsurf <bryancgant@gmail.com>
2022-06-25 11:37:43 +0800

    added level loop

--- game.py
+++ game.py
@@ -1,2 +1,15 @@
-inp = input("what is the level?: ")
+while(True):
+    try:
+        inp = int(input("what is the level?: "))
+    except:
+        print("must be a number")
+        continue
+
+    if inp < 1:
+        print("must be positive integer!")
+    else:
+        break
+   
+
+
 print(inp)
